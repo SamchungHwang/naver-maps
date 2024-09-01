310c0a6fa629a50aebc0be9a7956fa8a2adc4a29
Hwang SamChung <hwangsamchung@gmail.com>
2024-09-01 13:13:36 +0900

    update

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,53 @@
-const express = require("express");
+import express, { Request, Response } from "express";
+import axios from "axios";
+import { Server } from "http";  // 추가된 부분
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+app.get("/get-directions", async (req: Request, res: Response) => {
+  const { start, goal, option } = req.query;
 
-module.exports = app;
+  console.log("Query parameters:", { start, goal, option });
+
+  const url = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving";
+  const headers = {
+    "X-NCP-APIGW-API-KEY-ID": process.env.NAVER_CLIENT_ID,
+    "X-NCP-APIGW-API-KEY": process.env.NAVER_CLIENT_SECRET,
+  };
+
+  console.log("Request URL:", url);
+  console.log("Request headers:", headers);
+
+  try {
+    const response = await axios.get(url, {
+      params: { start, goal, option },
+      headers: headers,
+    });
+
+    console.log("Response status:", response.status);
+    console.log("Response data:", response.data);
+
+    res.status(200).json(response.data);
+  } catch (error: any) {
+    console.error("Error:", error.message);
+    if (error.response) {
+      console.error("Error response:", error.response.data);
+    }
+    res.status(error.response ? error.response.status : 500).json({
+      error: error.message,
+      details: error.response ? error.response.data : null,
+    });
+  }
+});
+
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 0; // This will choose a random available port
+  const server: Server = app.listen(PORT, () =>  // 타입 명시
+    console.log(`Server ready on port ${(server.address() as any).port}.`)
+  );
+}
+
+export default app;
